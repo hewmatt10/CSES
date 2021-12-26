94e9ec5d7a4b434651c17b4a33a9878c8e073059
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2021-12-25 23:58:59 -0500

    created git ignore

--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-        // for(int x : s){
-        //     if(i % 2 == 0){
-        //         cout << x + 1 << " ";
-        //         s.erase(s.lower_bound(x));
-        //     }
-        //     i = (i+1)%2;
-        // }
